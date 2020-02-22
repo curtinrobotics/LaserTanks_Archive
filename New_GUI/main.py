c39aaf7f2cb79c5b8e268f1808636a976a178ef0
Adeepa-1409 <adeepa@LAPTOP-UF8O59VM.localdomain>
2020-02-22 20:59:17 +0800

    master

--- New_GUI/main.py
+++ New_GUI/main.py
@@ -1,6 +1,8 @@
 from flask import *
+from forms import *
 
 app = Flask(__name__)
+app.secret_key = 'development key'
 
 @app.route("/",methods = ['POST', 'GET'])
 def home():
@@ -14,13 +16,28 @@ def home():
 
    return render_template("home.html")
 
-@app.route("/about")
+@app.route("/about", methods = ['GET', 'POST'])
 def about():
-    return render_template("about.html")
+   return render_template("about.html")
 
 @app.route('/success/<name>')
 def success(name):
    return 'welcome %s' % name
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+   form = ContactForm()
+   
+   if request.method == 'POST':
+      if form.validate() == False:
+         flash('All fields are required.')
+         return render_template('contact.html', form = form)
+      else:
+         print(form.name)
+         return render_template('about.html')
+   else:
+      return render_template('contact.html', form = form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
