c0d2a81cc71ad1e9f61d88974a5f2ac75da62af2
Adeepa-1409 <adeepa@LAPTOP-UF8O59VM.localdomain>
2020-02-22 16:31:16 +0800

    master

--- New_GUI/main.py
+++ New_GUI/main.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ['POST', 'GET'])
 def home():
-    print("hello")
-    return render_template("home.html")
-    
+   print("hello")
+   if request.method == 'POST':
+      if request.form.get('submit') == 'submit':
+         user = request.form['nm']
+         return redirect(url_for('success',name = user))
+   else:
+      return render_template("home.html")
+
+   return render_template("home.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-    
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
 if __name__ == "__main__":
     app.run(debug=True)
