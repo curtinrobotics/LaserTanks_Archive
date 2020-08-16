62d164990c4e257a41c49a0aac353bd53b2a463e
sixreals <ryanjwalton94@gmail.com>
2020-08-16 12:14:24 +0800

    updated schema,added comments

--- New_GUI/flaskr/schema.sql
+++ New_GUI/flaskr/schema.sql
@@ -1,11 +1,20 @@
 CREATE TABLE player (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT UNIQUE NOT NULL,
-    lives INTEGER,
-    kills INTEGER,
+    totalLives INTEGER,
+    totalKills INTEGER,
+    highScore INTEGER,
+    totalScore INTEGER,
     deaths INTEGER
 );
 
+/*A table containing entries of running games*/
 CREATE TABLE game (
-    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    playerIds TEXT NOTNULL,
+    startTime INTEGER NOTNULL,
+    endTime INTEGER,
+    gameType TEXT,
+    numPlayers INTEGER NOTNULL,
+    players TEXT NOTNULL /*an array of layers serialised as a json*/
 );
